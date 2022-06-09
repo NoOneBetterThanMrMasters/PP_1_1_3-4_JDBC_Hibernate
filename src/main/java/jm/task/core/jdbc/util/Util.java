c0636b72b1cca6dbe5b5205b5e4b980c074a115f
NoOneBetterThanMrMasters <verdant6th@gmail.com>
2022-06-09 19:13:42 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Ca:m5(d-_Fzba*6";
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() {
+        return connection;
+    }
 }
