024b2aa2c3a80478f857d6a893e029f961d40166
NoOneBetterThanMrMasters <verdant6th@gmail.com>
2022-06-13 23:02:03 +0300

    added factory

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
     private static final String USER = "root";
     private static final String PASSWORD = "Ca:m5(d-_Fzba*6";
     private static Connection connection;
+    private static final SessionFactory SESSION_FACTORY;
 
     static {
         try {
@@ -21,5 +27,26 @@ public class Util {
     }
     public static Connection getConnection() {
         return connection;
+    }
+
+    static {
+        try {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.connection.url", URL);
+            properties.setProperty("hibernate.connection.username", USER);
+            properties.setProperty("hibernate.connection.password", PASSWORD);
+            properties.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+
+            SESSION_FACTORY = new Configuration()
+                    .addAnnotatedClass(User.class)
+                    .setProperties(properties)
+                    .buildSessionFactory();
+
+        } catch (Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+    public SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
     }
 }
